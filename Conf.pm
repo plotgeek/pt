9c73f1c17fa43665feb01b9d138774fa5fcd031c
pisa <paizzj@126.com>
2025-03-16 08:08:30 +0800

    add stop flag for plot

--- Conf.pm
+++ Conf.pm
@@ -9,6 +9,7 @@ has $.p_threads        = 32;
 has $.f_threads        = 0;
 has $.m_threads        = 4;
 has $.nossd_level      = 33;
+has $.stop             = "--no-stop";
 has $.nossd            = "~/nossd/client";
 
 has $.nossd_mining     = False;
