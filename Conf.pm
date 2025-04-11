ca8f1cdb19d7f9f2fb49775da62ea54688f038db
pisa <paizzj@126.com>
2025-04-11 08:42:42 +0800

    rm unused conf args

--- Conf.pm
+++ Conf.pm
@@ -2,9 +2,6 @@ unit class Conf;
 
 # nossd args
 has $.addr             = "xch1xy4kmhd6avkde5z0h67mefzgakeq9ahnj802tuxjluq7prj9rhjqre2cjj"; # replace with your own addr
-has $.num_of_spt_disks = 5;
-has $.mem_spt          = "32G";
-has $.mem_fpt          = "4G";
 has $.p_threads        = 32;
 has $.f_threads        = 0;
 has $.m_threads        = 4;
@@ -12,14 +9,13 @@ has $.nossd_level      = 33;
 has $.stop             = "--no-stop"; # "--no-stop" or ""
 has $.nossd            = "$*HOME/pt/nossd/client";
 
-has $.nossd_mining     = True;
+has $.nossd_mining     = False;
 has $.notmp            = True;
 has $.use_cpu          = False;
 
 
 # common args
 has $.file_type        = "*.fpt";
-has $.disk_avail       = "80G";
 has $.type             = "pg"; # pg,og
 has $.mount_prefix     = "/"; # eg. '/' or  '/f1' or  'f1,f2,f3' 
 has $.plots_dir        = "plots"; # eg. "plots";
