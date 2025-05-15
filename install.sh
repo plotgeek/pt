1b9bc73ec29625c70f91a8b458811794e51241bc
pisa <paizzj@126.com>
2025-05-15 21:48:10 +0800

    set timezone

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
 sudo apt install tmux rakudo emacs-nox jq unzip gcc g++ cmake make lrzsz  smartmontools nfs-kernel-server  nfs-common -y
+echo "seting timezone Asia/Shanghai"
+sudo timedatectl set-timezone Asia/Shanghai
+echo "set timezone done"
